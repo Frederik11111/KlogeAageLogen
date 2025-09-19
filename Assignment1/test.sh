337fcec40ab01fcb0fd590ea03a044ff46bfc4ff
FrederikSchow <Frederikmorthorst123@yahoo.com>
2025-09-19 21:21:13 +0200

    added tests

--- Assignment1/test.sh
+++ Assignment1/test.sh
@@ -16,7 +16,21 @@ printf "Hello, World!\n" > test_files/ascii.input
 printf "Hello, World!" > test_files/ascii2.input
 printf "Hello,\x00World!\n" > test_files/data.input
 printf "" > test_files/empty.input
-### TODO: Generate more test files ###
+
+# ISO-8859-1 test (latin1 æøå)
+printf "Hællø\n" | iconv -f UTF-8 -t ISO-8859-1 > test_files/iso1.input
+printf "Smørrebrød\n" | iconv -f UTF-8 -t ISO-8859-1 > test_files/iso2.input
+
+
+
+# UTF-8 test med emojis og kinesiske tegn
+printf "Hello 🌍\n" > test_files/utf81.input
+printf "中文测试\n" > test_files/utf82.input
+
+# Flere datafiler
+dd if=/dev/zero of=test_files/data2.input bs=1 count=10 2>/dev/null
+printf "\xFF\xFE\xFD\xFC" > test_files/data.input
+
 
 
 echo "Running the tests.."
@@ -24,11 +38,14 @@ exitcode=0
 for f in test_files/*.input
 do
   echo ">>> Testing ${f}.."
-  file    ${f} | sed -e 's/ASCII text.*/ASCII text/' \
-                         -e 's/UTF-8 Unicode text.*/UTF-8 Unicode text/' \
-                         -e 's/ISO-8859 text.*/ISO-8859 text/' \
-                         -e 's/writable, regular file, no read permission/cannot determine (Permission denied)/' \
-                         > "${f}.expected"
+  file ${f} | sed -e 's/ASCII text.*/ASCII text/' \
+                  -e 's/UTF-8 Unicode text.*/UTF-8 Unicode text/' \
+                  -e 's/Unicode text, UTF-8 text.*/UTF-8 Unicode text/' \
+                  -e 's/ISO-8859 text.*/ISO-8859 text/' \
+                  -e 's/Unicode text, UTF-16.*/data/' \
+                  -e 's/writable, regular file, no read permission/cannot determine (Permission denied)/' \
+                  > "${f}.expected"
+
   ./file  "${f}" > "${f}.actual"
 
   if ! diff -u "${f}.expected" "${f}.actual"
