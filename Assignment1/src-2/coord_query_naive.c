a0c424f685e7f273e8767670ce03d1fd6e9bfec4
Frederik <ghw695@alumni.ku.dk>
2025-10-01 22:43:41 +0200

    coord query naive added

--- Assignment1/src-2/coord_query_naive.c
+++ Assignment1/src-2/coord_query_naive.c
@@ -10,23 +10,41 @@
 #include "coord_query.h"
 
 struct naive_data {
-  struct record *rs;
+  const struct record *rs;
   int n;
 };
 
-struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+struct naive_data* mk_naive(const struct record* rs, int n) {
+  struct naive_data *d = malloc(sizeof(struct naive_data));
+if (!d) {
+    fprintf(stderr, "Failed to allocate memory for naive_data (errno: %s)\n", strerror(errno));
+    exit(1);
+}
+  d->rs = rs;
+  d->n = n;
+  return d;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  free(data);
 }
 
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
+  const struct record* best = NULL;
+  double best_dist = 1e308; // A very large number
+
+  for (int i = 0; i < data->n; i++) {
+    const struct record *r = &data->rs[i];
+    double dx = r->lon - lon;
+    double dy = r->lat - lat;
+    double dist = dx*dx + dy*dy; // We can compare squared distances
+    if (dist < best_dist) {
+      best_dist = dist;
+      best = r;
+    }
+  }
+
+  return best;
 }
 
 int main(int argc, char** argv) {
