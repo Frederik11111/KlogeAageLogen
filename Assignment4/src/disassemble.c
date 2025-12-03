63aa5c5961402ad9f3ae7d4c6a6cdfe7c83916b5
Jaso0116 <144026089+Jaso0116@users.noreply.github.com>
2025-12-03 21:42:31 +0100

    disassembler done

--- Assignment4/src/disassemble.c
+++ Assignment4/src/disassemble.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Array to map register numbers (0-31) to names ("zero", "ra", "sp", etc.)
+// Array to map register numbers
 const char* reg_names[32] = {
     "zero", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
     "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -18,10 +18,15 @@ void disassemble(uint32_t addr, uint32_t instruction, char* result, size_t buf_s
     uint32_t rs2 = RS2(instruction);
     uint32_t funct3 = FUNCT3(instruction);
     uint32_t funct7 = FUNCT7(instruction);
+    int32_t imm = IMM_I(instruction);
+    uint32_t shamt = IMM_I(instruction) & 0x1F;
+    int32_t simm = IMM_S(instruction);
+    int32_t bimm = IMM_B(instruction);
+    uint32_t target = addr + bimm;  
+
 
     switch (opcode) {
         case 0x33: // R-Type
-            // Distinguish based on funct3 first
             switch (funct3) {
                 case 0x0: 
                     if (funct7 == 0x00) { // ADD
@@ -84,10 +89,95 @@ void disassemble(uint32_t addr, uint32_t instruction, char* result, size_t buf_s
                     }
                     break;
                 }
-        case 0x13: // I-Type
-            switch (funct3){
-                case 0x0:{ // ADDI
-                    sprintf(result, "and %s, %s, %s", reg_names[rd], reg_names[rs1], reg_names[rs2]);
-                }
+            break;
+            case 0x13:{ // I-Type
+                switch (funct3) {
+                case 0x0: // ADDI
+                    sprintf(result, "addi %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
+                
+                case 0x1: // SLLI
+                    sprintf(result, "slli %s, %s, %d", reg_names[rd], reg_names[rs1], shamt);
+                    break;
+                
+                case 0x2: // SLTI
+                    sprintf(result, "slti %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
+                
+                case 0x3: // SLTIU 
+                    sprintf(result, "sltiu %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
+                
+                case 0x4: // XORI
+                    sprintf(result, "xori %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
+                
+                case 0x5:
+                    if (funct7 == 0x00) { // SRLI
+                        sprintf(result, "srli %s, %s, %d", reg_names[rd], reg_names[rs1], shamt);
+                    } else if (funct7 == 0x20) { // SRAI
+                        sprintf(result, "srai %s, %s, %d", reg_names[rd], reg_names[rs1], shamt);
+                    }
+                    break;
+                
+                case 0x6: // ORI
+                    sprintf(result, "ori %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
+                
+                case 0x7: // ANDI
+                    sprintf(result, "andi %s, %s, %d", reg_names[rd], reg_names[rs1], imm);
+                    break;
             }
+            break;  
+            }
+            case 0x03: // I-Type
+                switch (funct3) {
+                case 0x0: sprintf(result, "lb %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]); break;
+                case 0x1: sprintf(result, "lh %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]); break;
+                case 0x2: sprintf(result, "lw %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]); break;
+                case 0x4: sprintf(result, "lbu %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]); break;
+                case 0x5: sprintf(result, "lhu %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]); break;
+            }
+            break;
+            case 0x23: // S-Type
+            {
+                switch (funct3) {
+                case 0x0: sprintf(result, "sb %s, %d(%s)", reg_names[rs2], simm, reg_names[rs1]); break;
+                case 0x1: sprintf(result, "sh %s, %d(%s)", reg_names[rs2], simm, reg_names[rs1]); break;
+                case 0x2: sprintf(result, "sw %s, %d(%s)", reg_names[rs2], simm, reg_names[rs1]); break;
+            }
+            break;
+            }
+            case 0x63: // B-Type
+            {
+                switch (funct3) {
+                case 0x0: sprintf(result, "beq %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+                case 0x1: sprintf(result, "bne %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+                case 0x4: sprintf(result, "blt %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+                case 0x5: sprintf(result, "bge %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+                case 0x6: sprintf(result, "bltu %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+                case 0x7: sprintf(result, "bgeu %s, %s, %x", reg_names[rs1], reg_names[rs2], target); break;
+            }
+            break;
+            }
+            case 0x37: // U-Type
+            {
+                uint32_t uimm = IMM_U(instruction) >> 12;
+                sprintf(result, "lui %s, 0x%x", reg_names[rd], uimm);
+                break;}
+            case 0x17: // U-Type
+            {
+                uint32_t uimm = IMM_U(instruction) >> 12;
+                sprintf(result, "auipc %s, 0x%x", reg_names[rd], uimm);
+                break;}
+            case 0x6F: //J-Type 
+            {
+                int32_t jimm = IMM_J(instruction);
+                uint32_t target = addr + jimm;
+                sprintf(result, "jal %s, 0x%x", reg_names[rd], target);
+                break; }
+            case 0x67: // I-Type    
+                sprintf(result, "jalr %s, %d(%s)", reg_names[rd], imm, reg_names[rs1]);
+                break;
+    }
 }
--- Assignment4/src/extractor.h
+++ Assignment4/src/extractor.h
@@ -6,3 +6,16 @@
 #define RS1(inst)      ((inst >> 15) & 0x1F)
 #define RS2(inst)      ((inst >> 20) & 0x1F)
 #define FUNCT7(inst)   ((inst >> 25) & 0x7F)
+#define IMM_I(inst)    ((int32_t)inst >> 20) //Immediat int
+#define IMM_S(inst)   ( (int32_t)(inst & 0xFE000000) >> 20 ) | ( (inst >> 7) & 0x1F )
+#define IMM_B(inst)   ( \
+    ((int32_t)(inst & 0x80000000) >> 19) | \
+    ((inst & 0x80) << 4) | \
+    ((inst >> 20) & 0x7E0) | \
+    ((inst >> 7) & 0x1E) )
+#define IMM_U(inst)   (inst & 0xFFFFF000)
+#define IMM_J(inst)   ( \
+    ((int32_t)(inst & 0x80000000) >> 11) | \
+    (inst & 0x000FF000) | \
+    ((inst & 0x00100000) >> 9) | \
+    ((inst & 0x7FE00000) >> 20) )
