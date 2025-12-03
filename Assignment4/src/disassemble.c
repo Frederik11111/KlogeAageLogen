974d660ad94b6cd463c876fcc6a306d3a0a0051e
Jaso0116 <144026089+Jaso0116@users.noreply.github.com>
2025-12-03 13:52:45 +0100

    comments

--- Assignment4/src/disassemble.c
+++ Assignment4/src/disassemble.c
@@ -1,10 +1,11 @@
 #include "disassemble.h"
+#include "extractor.h"
 #include <stdio.h>
 #include <string.h>
 
-// Macros for extraction (as discussed)
-#define OPCODE(inst)   (inst & 0x7F)
-#define RD(inst)       ((inst >> 7) & 0x1F)
+// Macros for extraction
+#define OPCODE(inst)   (inst & 0x7F) // Mask anyting but the last 7 bits
+#define RD(inst)       ((inst >> 7) & 0x1F) // Shift the first 7, and then mask intill last 5 bits
 #define FUNCT3(inst)   ((inst >> 12) & 0x7)
 #define RS1(inst)      ((inst >> 15) & 0x1F)
 #define RS2(inst)      ((inst >> 20) & 0x1F)
--- Assignment4/src/extractor.h
+++ Assignment4/src/extractor.h
@@ -1,9 +1,8 @@
-// Helper macros to extract fields from a 32-bit instruction
-// See RISC-V Green Card for these bit positions
+// Macros for extraction
 
-#define GET_OPCODE(inst)  ((inst) & 0x0000007F)
-#define GET_RD(inst)      (((inst) >> 7)  & 0x0000001F)
-#define GET_FUNCT3(inst)  (((inst) >> 12) & 0x00000007)
-#define GET_RS1(inst)     (((inst) >> 15) & 0x0000001F)
-#define GET_RS2(inst)     (((inst) >> 20) & 0x0000001F)
-#define GET_FUNCT7(inst)  (((inst) >> 25) & 0x0000007F)
+#define OPCODE(inst)   (inst & 0x7F) // Mask anyting but the last 7 bits
+#define RD(inst)       ((inst >> 7) & 0x1F) // Shift the first 7, and then mask intill last 5 bits
+#define FUNCT3(inst)   ((inst >> 12) & 0x7)
+#define RS1(inst)      ((inst >> 15) & 0x1F)
+#define RS2(inst)      ((inst >> 20) & 0x1F)
+#define FUNCT7(inst)   ((inst >> 25) & 0x7F)
